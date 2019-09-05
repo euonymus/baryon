57fcbafe8af97928843daeae63490f8db3e7e35a
euonymus <euonymus0220@gmail.com>
2019-09-06 04:18:25 +0900

    optimization

--- src/baryon-components/gluon.js
+++ src/baryon-components/gluon.js
@@ -1,32 +1,42 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import './assets/styles/baryon.css'
 // Material UI
+import { makeStyles } from '@material-ui/styles'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
 import ListItemAvatar from '@material-ui/core/ListItemAvatar'
 import Avatar from '@material-ui/core/Avatar'
 
-class Gluon extends Component {
-  render () {
-    const { interaction } = this.props
-    const { gluon, object } = interaction
-    return (
-      <div className="baryon-gluon-body">
-        <ListItem>
-          <Link to={`/${object.properties.name}`}>
-            <ListItemAvatar>
-              <Avatar>
-                <img className="baryon-gluon-image" src={object.properties.image_path} alt={object.properties.name} />
-              </Avatar>
-            </ListItemAvatar>
-          </Link>
-          <ListItemText primary={interaction.relationText} secondary={gluon.period_str} />
-        </ListItem>
-      </div>
-    )
-  }
+const useStyles = makeStyles({
+  avatarListItem: {
+    width: 150,
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+  },
+})
+
+const Gluon = (props) => {
+  const { interaction } = props
+  const { gluon, object } = interaction
+  const classes = useStyles()
+  return (
+    <div className="baryon-gluon-body">
+      <ListItem>
+        <Link to={`/${object.properties.name}`}>
+          <ListItemAvatar className={classes.avatarListItem} >
+            <Avatar className={classes.avatar} >
+              <img className="baryon-gluon-image" src={object.properties.image_path} alt={object.properties.name} />
+            </Avatar>
+          </ListItemAvatar>
+        </Link>
+        <ListItemText primary={interaction.relationText} secondary={gluon.period_str} />
+      </ListItem>
+    </div>
+  )
 }
 
 Gluon.propTypes = {
