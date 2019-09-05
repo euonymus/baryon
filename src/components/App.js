5b85ec084b9103522b969c423680715ed435981b
euonymus <euonymus0220@gmail.com>
2019-09-05 10:37:02 +0900

    initialization

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../assets/images/logo.svg';
+import '../assets/styles/App.css';
 
 function App() {
   return (
