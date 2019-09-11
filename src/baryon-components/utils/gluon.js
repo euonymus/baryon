834e998d6750a5232e30676967af833604a0c90f
euonymus <euonymus0220@gmail.com>
2019-09-11 23:18:08 +0900

    optimization

--- src/baryon-components/utils/gluon.js
+++ src/baryon-components/utils/gluon.js
@@ -1,4 +1,5 @@
 import Util from './common'
+import { gluon_types } from '../constants/gluon_types'
 import { LANGTYPE_ENG_LIKE, LANGTYPE_JP_LIKE } from '../constants/langtypes'
 class GluonUtil {
   constructor(gluonRaw, langType = LANGTYPE_ENG_LIKE) {
@@ -13,10 +14,15 @@ class GluonUtil {
   }
 
   getRelation = () => {
+    let relation = 'has relation to'
+    if (gluon_types[this.type]) {
+      relation = gluon_types[this.type].caption
+    }
     if (this.langType === LANGTYPE_JP_LIKE) {
-      return this.properties.relation ? this.properties.relation : this.type.replace(/_/g, ' ').toLowerCase()
+      return this.properties.relation ? this.properties.relation : relation
     } else {
-      return this.type ? this.type.replace(/_/g, ' ').toLowerCase(): this.properties.relation
+      // return this.type ? this.type.replace(/_/g, ' ').toLowerCase(): this.properties.relation
+      return this.type ? relation: this.properties.relation
     }
   }
 }
