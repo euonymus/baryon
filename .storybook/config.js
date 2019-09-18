73217c1cc90d7ce588d03fe7af6d59b68182c0f4
euonymus <euonymus0220@gmail.com>
2019-09-18 23:43:45 +0900

    storybook init

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,6 @@
 import { configure } from '@storybook/react';
+import requireContext from 'require-context.macro'
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+// configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+configure(requireContext('../src/stories', true, /\.stories\.js$/), module);
