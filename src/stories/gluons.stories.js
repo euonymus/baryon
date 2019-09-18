d9088d59cecd5602fb16b0a45cce85d730f53dd3
euonymus <euonymus0220@gmail.com>
2019-09-19 02:05:01 +0900

    try

--- src/stories/gluons.stories.js
+++ src/stories/gluons.stories.js
@@ -6,19 +6,21 @@ import Gluons from '../baryon-components/gluons'
 import { interaction } from './gluon.stories'
 import { LANGTYPE_JP_LIKE } from '../baryon-components/constants/langtypes'
 
+class Record {
+  keys = ['subject', 'gluon', 'object']
+  _fieldLookup = { subject:'0', gluon:'1', object: '2' }
+  _fields = {
+    '0': { identity: {low:192, high: 0}, labels: ['Movie'], properties: {name:'hoge', start:'2000-01-01'}},
+    '1': { identity: {low:192, high: 0}, type: 'Directed', properties: {relation:'hoge', start:'2000-01-01'}},
+    '2': { identity: {low:192, high: 0}, labels: ['Movie'], properties: {name:'hoge', start:'2000-01-01'}},
+  }
+  get = (key) => {
+    return this._fields[key]
+  }
+}
+const record = new Record()
 export const gluons = [
-  {
-    keys: ['subject', 'gluon', 'object'],
-    _fieldLookup: { subject:'0', gluon:'1', object: '2' },
-    _fields: {
-      '0': { identity: {low:192, high: 0}, labels: ['Movie'], properties: {name:'hoge'}},
-      '1': {},
-      '2': {},
-    },
-    get: (key) => {
-      return this._fields[key]
-    },
-  },
+  record,
 ]
 export const langType = LANGTYPE_JP_LIKE
 
