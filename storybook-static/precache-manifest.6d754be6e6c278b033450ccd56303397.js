d050cbe7d9f85abbdabf0a514ca19aa72a12d4f9
euonymus <euonymus0220@gmail.com>
2019-09-19 11:09:16 +0900

    storybook-static

---
+++ storybook-static/precache-manifest.6d754be6e6c278b033450ccd56303397.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6fb712f0dfd0e3751518f118ee65db2",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.40c63c03fd2b1ec94a40.bundle.js"
+  },
+  {
+    "revision": "fe7c08882197b4e32949",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.40c63c03fd2b1ec94a40.bundle.js"
+  },
+  {
+    "url": "vendors~main.40c63c03fd2b1ec94a40.bundle.js"
+  }
+]);
