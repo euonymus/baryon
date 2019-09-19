5ca0175807a4a1cfd0afd78f9abc42fec0404e68
euonymus <euonymus0220@gmail.com>
2019-09-19 11:40:16 +0900

    main-quark storybook

---
+++ src/stories/main-quark.stories.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { storiesOf } from '@storybook/react'
+
+import MainQuark from '../baryon-components/main-quark'
+
+export const subject = {
+ 	name: 'The Matrix',
+ 	description: 'This is a movie',
+ 	image_path: 'https://images.ciatr.jp/2017/07/w_828/0kDnZaTrd4xrWWav8PB8XIVBRXoQGsWGgNSdnp2E.jpeg',
+ 	period_str: '(2019-10-16 - 2019-12-15)',
+ 	url: 'http://www.whatisthematrix.com',
+ 	affiliate: 'http://www.whatisthematrix.com'
+}
+
+storiesOf('MainQuark', module)
+  .add('default', () => <MainQuark subject={subject} />)
