dd77a662a75321dd47d3d733168ad9d40045c5d8
euonymus <euonymus0220@gmail.com>
2019-09-19 11:19:28 +0900

    chromatic

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,6 @@
 import { configure } from '@storybook/react';
 import requireContext from 'require-context.macro'
+import 'storybook-chromatic';
 
 // automatically import all files ending in *.stories.js
 // configure(require.context('../src/stories', true, /\.stories\.js$/), module);
